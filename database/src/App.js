1a39268ad217ae3c6bac8cce58e1d3f23da702b7
cmrnprry <perry.ca@husky.neu.edu>
2020-02-10 15:29:58 -0500

    ahhhh

--- database/src/App.js
+++ database/src/App.js
@@ -9,6 +9,26 @@ import Home from './Pages/Home';
 import Graphs from './Pages/Graphs';
 
 
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host: process.env.game-job-database-db-instance.cwzfnaatoex3.us-east-2.rds.amazonaws.com,
+  user: process.env.gamejobadmin,
+  password: process.env.Becker2047,
+  port: process.env.3306
+});
+
+connection.connect(function (err) {
+  if (err) {
+    console.error('Database connection failed: ' + err.stack);
+    return;
+  }
+
+  console.log('Connected to database.');
+});
+
+connection.end();
+
 class App extends Component {
   render() {
     return (
