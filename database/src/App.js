e4282ab03554765848ac57a6f45b30f72c8050c8
cmrnprry <perry.ca@husky.neu.edu>
2020-02-24 13:36:42 -0500

    i don't understand why this isn't working

--- database/src/App.js
+++ database/src/App.js
@@ -9,28 +9,29 @@ import Home from './Pages/Home';
 import Graphs from './Pages/Graphs';
 
 
-var mysql = require('mysql');
-
-var connection = mysql.createConnection({
-  host: process.env.game-job-database-db-instance.cwzfnaatoex3.us-east-2.rds.amazonaws.com,
-  user: process.env.gamejobadmin,
-  password: process.env.Becker2047,
-  port: process.env.3306
-});
-
-connection.connect(function (err) {
-  if (err) {
-    console.error('Database connection failed: ' + err.stack);
-    return;
-  }
-
-  console.log('Connected to database.');
-});
-
-connection.end();
-
 class App extends Component {
   render() {
+
+    // var mysql = require('mysql');
+
+    // var connection = mysql.createConnection({
+    //   host: process.env.RDS_HOSTNAME,
+    //   user: process.env.RDS_USERNAME,
+    //   password: process.env.RDS_PASSWORD,
+    //   port: process.env.RDS_PORT
+    // });
+
+    // connection.connect(function (err) {
+    //   if (err) {
+    //     console.error('Database connection failed: ' + err.stack);
+    //     return;
+    //   }
+
+    //   console.log('Connected to database.');
+    // });
+
+    // connection.end();
+
     return (
       <Router>
         <div>
