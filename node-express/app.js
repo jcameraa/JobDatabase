10d2f534241c9fc16d5ce1cbe4c46fc730b7c436
cmrnprry <perry.ca@husky.neu.edu>
2020-02-24 20:19:40 -0500

    Add database

--- node-express/app.js
+++ node-express/app.js
@@ -9,6 +9,29 @@ var usersRouter = require('./routes/users');
 var hike = require('./routes/hike');
 
 var app = express();
+
+
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host: process.env.RDS_HOSTNAME,
+  user: process.env.RDS_USERNAME,
+  password: process.env.RDS_PASSWORD,
+  port: process.env.RDS_PORT
+});
+
+connection.connect(function (err) {
+  if (err) {
+    console.error('Database connection failed: ' + err.stack);
+    return;
+  }
+
+  console.log('Connected to database.');
+});
+
+connection.end();
+
+
 app.get('/hikes', hike.index);
 app.post('/add_hike', hike.add_hike);
 
